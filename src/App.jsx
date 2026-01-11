5f2289ee6b2a79239b46475e6bf6df96bc8c9e79
ALI RAZA <razadeveloper816@gmail.com>
2026-01-10 17:23:05 -0800

    complete portfolio section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import About from './Component/About'
 import Header from './Component/Header'
 import HeroSection from './Component/HeroSection'
+import Portfolio from './Component/Portfolio'
 import Service from './Component/Service'
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
     <HeroSection/>
     <Service/>
     <About/>
+    <Portfolio/>
     </>
   )
 }
---
+++ src/Component/Portfolio.jsx
@@ -0,0 +1,74 @@
+import style from '../css/Portfolio.module.css';
+
+export default function Portfolio() {
+    return (
+        <div className={style.portfolio}>
+            <div className={style.wrapper}>
+                <h1>Portfolio</h1>
+                <p>A curated showcase of innovative projects where clean code meets creative design.</p>
+                <div className={style.projectsCollection}>
+                    <div className={style.cards}>
+                        <div style={{position:'relative'}} className={style.img}>
+                            <img src="/images/Rectangle 21.png" alt="" />
+                        </div>
+                        <div className={style.btns}>
+                            <button>Live Demo</button>
+                            <button>GitHub</button>
+                        </div>
+                        <h1><span>Forever</span><span>e-commerce</span></h1>
+                    </div>
+                    <div className={style.cards}>
+                        <div style={{position:'relative'}} className={style.img}>
+                            <img src="/images/Rectangle 21.png" alt="" />
+                        </div>
+                        <div className={style.btns}>
+                            <button>Live Demo</button>
+                            <button>GitHub</button>
+                        </div>
+                        <h1><span>Forever</span><span>e-commerce</span></h1>
+                    </div>
+                    <div className={style.cards}>
+                        <div style={{position:'relative'}} className={style.img}>
+                            <img src="/images/Rectangle 21.png" alt="" />
+                        </div>
+                        <div className={style.btns}>
+                            <button>Live Demo</button>
+                            <button>GitHub</button>
+                        </div>
+                        <h1><span>Forever</span><span>e-commerce</span></h1>
+                    </div>
+                    <div className={style.cards}>
+                        <div style={{position:'relative'}} className={style.img}>
+                            <img src="/images/Rectangle 21.png" alt="" />
+                        </div>
+                        <div className={style.btns}>
+                            <button>Live Demo</button>
+                            <button>GitHub</button>
+                        </div>
+                        <h1><span>Forever</span><span>e-commerce</span></h1>
+                    </div>
+                    <div className={style.cards}>
+                        <div style={{position:'relative'}} className={style.img}>
+                            <img src="/images/Rectangle 21.png" alt="" />
+                        </div>
+                        <div className={style.btns}>
+                            <button>Live Demo</button>
+                            <button>GitHub</button>
+                        </div>
+                        <h1><span>Forever</span><span>e-commerce</span></h1>
+                    </div>
+                    <div className={style.cards}>
+                        <div style={{position:'relative'}} className={style.img}>
+                            <img src="/images/Rectangle 21.png" alt="" />
+                        </div>
+                        <div className={style.btns}>
+                            <button>Live Demo</button>
+                            <button>GitHub</button>
+                        </div>
+                        <h1><span>Forever</span><span>e-commerce</span></h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
