73d367c62bf11a2bd02de55c98f1293410f88f50
ALI RAZA <razadeveloper816@gmail.com>
2026-01-10 16:44:47 -0800

    complete about section

--- src/Component/About.jsx
+++ src/Component/About.jsx
@@ -11,35 +11,40 @@ export default function About() {
                         <img src="/images/about-img.png" alt="" />
                     </div>
                     <div className={style.rightBox}>
-                        <p>I am a skilled web developer with over 8 months experience in the industry. My passion lies in creatubg captivating website designs and implementing them through fronted development. I take pride in staying up-to-date with current design trends and leveraging my creativity to produce visually appealing and user-friendly websites. Throughout my career, I have developed a deep understanding of user experience and user interface principles. By putting myself in the shoes of the end-users, I strive to create intuitive and seamless browsing experience. I believe that a well-designed website should not only look visually appealing but also provide a smooth and enjouable interaction for visitors. When starting a new project, I thoroughly research and analyze the target audience and the client's specific requirements. This enables me to tailor my designs to meet their expectations and deliver a unique online presence that aligns with their brand indentity.</p>
+                        <p>I am a skilled <b style={{color:'#fff',fontSize:'16px'}} >React Developer</b> with over 8 months experience in the industry. My passion lies in creatubg captivating website designs and implementing them through fronted development. I take pride in staying up-to-date with current design trends and leveraging my creativity to produce visually appealing and user-friendly websites. Throughout my career, I have developed a deep understanding of user experience and user interface principles. By putting myself in the shoes of the end-users, I strive to create intuitive and seamless browsing experience. I believe that a well-designed website should not only look visually appealing but also provide a smooth and enjouable interaction for visitors. When starting a new project, I thoroughly research and analyze the target audience and the client's specific requirements. This enables me to tailor my designs to meet their expectations and deliver a unique online presence that aligns with their brand indentity.</p>
                         <button><LuDownload /><span>Download CV</span></button>
                     </div>
                 </div>
                 <div className={style.skillStat}>
-                    <div className={style.box1}>
-                        <div className={style.circle1}></div>
-                        <span>100%</span>
+                    <div style={{textAlign:"center"}} className={`${style.box} ${style.box1} `}>
+                        <div className={`${style.circle} ${style.circle1}`}>
+                            <span>100%</span>
+                        </div>
                         <span>HTML</span>
                     </div>
-                    <div className={style.box1}>
-                        <div className={style.circle2}></div>
-                        <span>100%</span>
-                        <span>HTML</span>
+                    <div style={{textAlign:"center"}} className={`${style.box} ${style.box3} `}>
+                        <div className={`${style.circle} ${style.circle3}`}>
+                            <span>80%</span>
+                        </div>
+                        <span>Javascript</span>
                     </div>
-                    <div className={style.box1}>
-                        <div className={style.circle3}></div>
-                        <span>100%</span>
-                        <span>HTML</span>
+                     <div style={{textAlign:"center"}} className={`${style.box} ${style.box2} `}>
+                        <div className={`${style.circle} ${style.circle2}`}>
+                            <span>85%</span>
+                        </div>
+                        <span>CSS</span>
                     </div>
-                    <div className={style.box1}>
-                        <div className={style.circle4}></div>
-                        <span>100%</span>
-                        <span>HTML</span>
+                    <div style={{textAlign:"center"}} className={`${style.box} ${style.box4} `}>
+                        <div className={`${style.circle} ${style.circle4}`}>
+                            <span>90%</span>
+                        </div>
+                        <span>Bootstrap</span>
                     </div>
-                    <div className={style.box1}>
-                        <div className={style.circle5}></div>
-                        <span>100%</span>
-                        <span>HTML</span>
+                    <div style={{textAlign:"center"}} className={`${style.box} ${style.box5} `}>
+                        <div className={`${style.circle} ${style.circle5}`}>
+                            <span>75%</span>
+                        </div>
+                        <span>React</span>
                     </div>
                 </div>
             </div>
